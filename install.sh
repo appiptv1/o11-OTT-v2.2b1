700c166a0164cf6196c71850b94fd1b7179f5a8e
appiptv <appiptv1@gmail.com>
2025-10-21 23:22:54 +0700

    Update service file URL in install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ apt install unrar -y
 wget https://github.com/sibuk76/o11-OTT-v2.2b1/raw/main/o11.rar
 unrar x o11.rar /home
 chmod -R +x /home/o11/
-wget https://raw.githubusercontent.com/sibuk76/o11-OTT-v2.2b1/main/o11.service
+wget https://raw.githubusercontent.com/appiptv1/o11-OTT-v2.2b1/refs/heads/main/o11.service
 chmod +x o11.service
 mv ./o11.service /etc/systemd/system/
 systemctl daemon-reload
